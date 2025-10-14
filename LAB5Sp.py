1b4804a5e7d7aa03d2a3b9666f9fd5cca6043a05
unknown <sangnmps39761@gmail.com>
2025-10-14 11:31:33 +0700

    nôp lab

---
+++ LAB5Sp.py
@@ -0,0 +1,15 @@
+class z:
+    def __init__(self, ten_sp, gia_sp,giam_gia):
+        self.ten_sp = ten_sp
+        self.gia_sp = gia_sp
+        self.giam_gia = giam_gia
+    def thue_nhap_khau(self):
+        return self.gia * 0.1
+    def xuat_thong_tin_SP(self):
+        print(f"sản phẩm {self.ten_sp}, giá {self.gia_sp}, giảm giá {self.giam_gia}")
+
+
+
+
+    
+    
